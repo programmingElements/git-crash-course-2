284397dcd3667103b5200f2a885840e76a4b6ecb
chakravarthi <chakrijallu7@gmail.com>
2024-02-13 14:48:56 +0530

    added product service file

---
+++ productService.js
@@ -0,0 +1,15 @@
+function addProduct(product) {
+    return product;
+}
+
+function editProduct(product, productId) {
+    return product;
+}
+
+function getProduct() {
+    return "Got Product Info";
+}
+
+function getAllProduct() {
+    return "Get All Products";
+}
