ef676fdc833a739a39529fc5ffeae80c172ec7c4
chakravarthi <chakrijallu7@gmail.com>
2024-02-13 14:51:39 +0530

    added order service

---
+++ orderService.js
@@ -0,0 +1,11 @@
+function createOrder(order) {
+    return order;
+}
+
+function getOrder() {
+    return "Order is confirmed"
+}
+
+function orderPayment() {
+    return "Order payment is processing"
+}
