906a7d1a12c9ea35a0731b7f8a4cf8949b2795c0
chakravarthi <chakrijallu7@gmail.com>
2024-03-07 10:14:51 +0530

    added createCategory in categoryservice file

--- categoriesService.js
+++ categoriesService.js
@@ -1,3 +1,9 @@
+const createCategory = (request, response) => {
+  return response.status(201).json({
+    message: "Category Created Successfully"
+  })
+}
+
 const getAllCategories = (request, response) => {
   return response.status(200).json({
     message: "Got All Categories"
